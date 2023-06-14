ad524cc24a8a9e5673544c7f1be88ea2787b7cd2
Jethro_Block <jethroblock22@gmail.COM>
2023-06-14 14:32:48 +0100

    Create Content in Skills

---
+++ src/Components/Skills.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div>
+    
+    <h2> Technical Skills </h2>
+    <h3>Front End</h3>
+   <ul>
+    <li>HTML</li>
+    <li>JavaScript</li>
+    <li>CSS</li>
+    <li>React</li>
+    <li>Ruby</li>
+   </ul>
+
+    <h3>Back End</h3>
+    <ul>
+        <li>Express</li>
+        <li>MongoDB</li>
+        <li>Mongoose</li>
+        <li>SQL / PostgreSQL</li>
+        <li>Rails</li>
+    </ul>
+
+    <h3>Other</h3>
+    <ul>
+        <li>CLI</li>
+        <li>GitHub</li>
+        <li>Devise</li>
+        <li>Passport</li>
+    </ul>
+
+    <h3>Soft Skills</h3>
+    <ul>
+        <li>Communication</li>
+        <li>Project/Time Management</li>
+        <li>Punctuality</li>
+        <li>Problem Solving</li>
+    </ul>
+    </div>
+  )
+}
+
+export default Skills
