ee9ab12d0946b750ad4128b73a4120525292660c
Jethro_Block <jethroblock22@gmail.COM>
2023-06-14 14:33:14 +0100

    Add Skills to App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import Contact from './Components/Contact';
 import Projects from './Components/Projects';
 import './App.css';
+import Skills from './Components/Skills';
 
 function App() {
   return (
     <div className="App">
       <Contact />
       <Projects />
+      <Skills />
     </div>
   );
 }
