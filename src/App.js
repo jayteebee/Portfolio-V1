184d0e428170860ecd2b0379c60c90ba908e576b
Jethro_Block <jethroblock22@gmail.COM>
2023-06-14 12:34:13 +0100

    Dir re-config Add components to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
+import Contact from './Components/Contact';
+import Projects from './Components/Projects';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Contact />
+      <Projects />
     </div>
   );
 }
