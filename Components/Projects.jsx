8da6a0b5b2c77db009b15561930eeb8518fb2af9
Jethro_Block <jethroblock22@gmail.COM>
2023-06-14 12:32:10 +0100

    Add Content to Projects

--- Components/Projects.jsx
+++ Components/Projects.jsx
@@ -4,7 +4,20 @@ const Projects = () => {
   return (
     <div>
     
-    
+    <h2> My Projects!</h2>
+    <h3>Project 1</h3>
+    <a href="http://github.com/jayteebee/Project-TTT">Tic Tac Toe - Repo</a>
+    <a href="https://jayteebee.github.io/Project-TTT/">Tic Tac Toe - Deployed</a>
+    <h3>Project 2</h3>
+    <a href="http://github.com/jayteebee/workout_planner">Workout Planner - Repo</a>
+    <a href="https://jayteebee.github.io/workout_planner/">Workout Planner - Deployed</a>
+    <h3>Project 3</h3>
+    <a href="http://github.com/jayteebee/Football-Friends">Football Friends - Repo</a>
+    <h3>Project 4</h3>
+    <a href="https://github.com/jayteebee/fantasyFinance">Fantasy Finance - Back End Repo</a>
+    <a href="https://github.com/jayteebee/FantasyFinanceFrontEnd">Fantasy Finance - Front End Repo</a>
+
+
     
     </div>
   )
