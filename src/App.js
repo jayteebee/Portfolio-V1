f6324d0e6799619fd1c8ca11ebeb3546320deed1
Jethro_Block <jethroblock22@gmail.COM>
2023-07-01 18:44:28 +0100

    padding

--- src/App.js
+++ src/App.js
@@ -7,17 +7,18 @@ import P4 from "./Components/Projects/P4";
 import NavBar from "./Components/NavBar";
 import AboutMe from "./Components/AboutMe";
 import Skills from "./Components/Skills";
-import tictactoe from "./Logos/tic-tac-toe.png"
-import dumbbell from "./Logos/dumbbells.png"
-import football from "./Logos/football.png"
-import money from "./Logos/money-bag.png"
-
+import tictactoe from "./Logos/tic-tac-toe.png";
+import dumbbell from "./Logos/dumbbells.png";
+import football from "./Logos/football.png";
+import money from "./Logos/money-bag.png";
 
 function App() {
   return (
     <div className="App">
       <div className="Container">
         <NavBar />
+
+        
         <div className="intro">
           <p>Hi, I'm</p>
         </div>
@@ -25,6 +26,7 @@ function App() {
           <p>Jethro Block,</p>
         </div>
         <div className="outro">A Junior Full Stack Developer.</div>
+     
 
         <div id="ContactScroll" className="Contact">
           <Contact />
@@ -33,21 +35,21 @@ function App() {
         <div id="AboutScroll" className="fc AboutMe">
           <AboutMe />
         </div>
-        
+
         <div id="ProjectScroll" className="fc P1">
           <P1 />
         </div>
 
-<div className="ttt">
-<img className="Logo" src={tictactoe} alt="tictactoe logo"></img>
-</div>
+        <div className="ttt">
+          <img className="Logo" src={tictactoe} alt="tictactoe logo"></img>
+        </div>
 
         <div className="fc P2">
           <P2 />
         </div>
 
         <div className="db">
-        <img className="Logo" src={dumbbell} alt="dumbbell logo"></img>
+          <img className="Logo" src={dumbbell} alt="dumbbell logo"></img>
         </div>
 
         <div className="fc P3">
@@ -55,7 +57,7 @@ function App() {
         </div>
 
         <div className="football">
-        <img className="Logo" src={football} alt="football logo"></img>
+          <img className="Logo" src={football} alt="football logo"></img>
         </div>
 
         <div className="fc P4">
@@ -63,13 +65,12 @@ function App() {
         </div>
 
         <div className="money">
-        <img className="Logo" src={money} alt="money logo"></img>
+          <img className="Logo" src={money} alt="money logo"></img>
         </div>
 
         <div id="SkillsScroll" className="fc Skills">
           <Skills />
         </div>
-
       </div>
     </div>
   );
