c8502df1c2d05f689b1f7b678ef87a8d989ea51f
Jethro_Block <jethroblock22@gmail.COM>
2023-09-15 15:38:20 +0100

    Update URL with https

--- src/Components/Projects/P3.jsx
+++ src/Components/Projects/P3.jsx
@@ -43,7 +43,7 @@ const P3 = () => {
         </a>
 
         <a
-        href="football-friends.vercel.app" // working
+        href="https://football-friends.vercel.app/" // working
         rel="noreferrer"
         target="_blank"
       >
